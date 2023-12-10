a9f8078d6fa891152c301540b6b3b2a411faccce
Mariia Postovik <accaunt01.13@gmail.com~>
2023-12-10 13:07:09 +0200

    map

---
+++ assets/blog-53d0c6cf.js
@@ -0,0 +1,6 @@
+import"./animate-28cfcd16.js";let t=document.getElementById("gamburger-menu-icon"),l=document.querySelector(".bottom-line-nav"),e=document.querySelector("nav");t.addEventListener("click",()=>{l.style.display==="flex"?(l.style.display="none",e.classList.add("animate__fadeOutLeft"),setTimeout(()=>{e.classList.remove("active"),e.classList.remove("animate__fadeOutLeft")},500),e.classList.remove("animate__fadeInLeft"),t.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                          <path d="M14 3H2V4H14V3Z" fill="#2A254B"/>
+                          <path d="M14 12H2V13H14V12Z" fill="#2A254B"/>
+                          <path d="M14 6H2V7H14V6Z" fill="#2A254B"/>
+                          <path d="M14 9H2V10H14V9Z" fill="#2A254B"/>
+                        </svg>`):(l.style.display="flex",e.classList.add("active"),e.classList.add("animate__fadeInLeft"),t.innerHTML='<ion-icon name="close-outline" class="close-nav"></ion-icon>')});let n=document.getElementById("close-pop-up__btn"),i=document.querySelector(".pop-up");n.addEventListener("click",()=>{i.style.display="none"});
